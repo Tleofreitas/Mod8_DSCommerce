0513c84b04de1544de60be15a26b93cd1362e0de
Thiago Freitas <thiagoleonardo1995@live.com>
2022-11-17 17:25:29 -0300

    DTO's

---
+++ src/main/java/com/devsuperiorT/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,59 @@
+package com.devsuperiorT.dscommerce.dto;
+
+import com.devsuperiorT.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
+
+// Carregar as informações do produto
+public class ProductDTO {
+    private Long id;
+    @Size(min= 3, max= 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String name;
+    @Size(min= 10, message = "A descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String description;
+    @Positive(message = "O preço deve ser positivo")
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+
+    // Macete para receber entidade direta
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    /* Em caso de entidades grandes,usar ModelMapper:
+    https://www.baeldung.com/entity-to-and-from-dto-for-a-java-spring-application
+     */
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
