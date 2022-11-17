f4d1331af21e5ec9ad740deb1165256414b5c031
Thiago Freitas <thiagoleonardo1995@live.com>
2022-11-17 17:26:46 -0300

    Services

---
+++ src/main/java/com/devsuperiorT/dscommerce/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.devsuperiorT.dscommerce.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg) {
+
+        super(msg);
+    }
+}
